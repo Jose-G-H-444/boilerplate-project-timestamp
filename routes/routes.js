262f4a1a8fe221f83aeddf1cf5ae2e4cb07208cc
Jose G <gallardohernandezjose444@gmail.com>
2021-10-23 18:55:55 -0700

    Add basic hello route

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const appController = require('../controllers/appController');
 
 // API Routes
+router.get("/api/hello", appController.hello);
 
 module.exports = router;
