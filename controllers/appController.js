529f7dbe24dcf2c32d02dc793c0f96960d263fd3
Jose G <gallardojose444@gmail.com>
2021-10-26 10:26:23 -0700

    Add compatibility for all Date formats

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,6 +1,4 @@
 const timestamp = require('unix-timestamp');
-const validator = require('is-my-date-valid')
-const validate = validator({ format: 'YYYY-MM-DD' })
 const dateTime = require('date-and-time');
 
 function isNumeric(str) {
@@ -13,7 +11,7 @@ exports.date = (req, res) => {
     const date = req.params.date;
     console.log(parseInt('944006400', 10));
     const format =  'ddd, DD MMM YYYY HH:mm:ss';
-    validate(date) ? res.json({ unix: timestamp.fromDate(new Date(date)) / 1000, utc: dateTime.format(new Date(date), format, true) + ' GMT'}) :
+    timestamp.fromDate(new Date(date)) ? res.json({ unix: timestamp.fromDate(new Date(date)) / 1000, utc: dateTime.format(new Date(date), format, true) + ' GMT'}) :
     isNumeric(date) ? res.json({ unix: parseInt(date, 10) / 1000, utc: dateTime.format(new Date(parseInt(date, 10) * 1000), format, true) + ' GMT'}) :
     res.send('INVALID INPUT, ENTER <UNIX-TIMESTAMP> OR <YYYY-MM-DD>');
 };
